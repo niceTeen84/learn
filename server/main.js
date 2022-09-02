0404d867637339cc5ab50713d22d16352ea4b6ea
bowen ren <armyknife84@163.com>
2022-09-02 10:39:08 +0800

    bug-fixed: express middleware order

--- server/main.js
+++ server/main.js
@@ -5,18 +5,23 @@ import { fileURLToPath } from 'url';
 
 const app = express()
 const svr = http.createServer(app)
+
 const bind = '0.0.0.0'
 const port = 4000
+
+// in ES module can not use `path.join(__dirname, 'static')` directly
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-app.use(express.static(path.join(__dirname, 'static')))
-
+// response header needs to set first
+// then to find static html file
 app.use((_, resp, next) => {
     resp.header('Cross-Origin-Opener-Policy', 'same-origin')
     resp.header('Cross-Origin-Embedder-Policy', 'require-corp')
     next()
 })
+
+app.use(express.static(path.join(__dirname, 'static')))
 
 svr.listen(port, bind, () => {
     console.log(`Server running at http://localhost:${port}/`)
