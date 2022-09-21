3865c11293471a35635811e0c9e8787f260e7056
bowen ren <armyknife84@163.com>
2022-09-21 17:16:35 +0800

    add: python decompress mysql compressed data

---
+++ python-demo/decompressed.py
@@ -0,0 +1,30 @@
+import zlib
+import pymysql
+from pymysql.cursors import DictCursor
+from dotenv import load_dotenv
+import os
+
+CHARSET = 'utf-8'
+
+fn_val = lambda k: os.getenv(k.upper())
+
+
+def main():
+    load_dotenv()
+    info = {k: fn_val(k) if k != 'port' else int(fn_val(k)) for k in ['host', 'user', 'password', 'database', 'port']}
+    conn = pymysql.connect(**info)
+    with conn:
+        with conn.cursor(DictCursor) as cur:
+            cur.execute("select compress(repeat('hello', 100)) res")
+            res = cur.fetchone()
+            # mysql compress
+            result = zlib.decompress(res['res'][4:]).decode(CHARSET)
+    # 对比
+    r = zlib.compress(b'hello' * 100)
+    print('bytes to hex: ', r.hex())
+    print('revert hex to bytes: ', bytes(bytearray.fromhex(r.hex())))
+    assert result == zlib.decompress(r).decode(CHARSET)
+
+
+if __name__ == '__main__':
+    main()
